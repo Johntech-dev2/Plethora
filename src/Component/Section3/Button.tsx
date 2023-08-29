39cd63bf3f5403f7aa1242423d2de86271cb3702
johntech-dev <johnayomide50@gmail.com>
2023-08-29 15:20:56 +0100

    updated

---
+++ src/Component/Section3/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <section>
+        <div className='btn'>
+            <h5>Are you looking for</h5>
+            <button type='button'></button>
+        </div>
+    </section>
+  )
+}
+
+export default Button
